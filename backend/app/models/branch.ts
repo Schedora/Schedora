21514c80a3586b137efa226546efc8f47e542b23
Claris Wega <nclaris10@gmail.com>
2026-09-18 08:25:06 +0300

    fix branch model column name mapping

--- backend/app/models/branch.ts
+++ backend/app/models/branch.ts
@@ -11,7 +11,7 @@ export default class Branch extends BaseModel {
   @column()
   declare businessId: number
 
-  @column()
+    @column({ columnName: 'branch_name' })
   declare name: string
 
   @column()
